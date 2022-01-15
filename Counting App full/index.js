bd82da2f180704a1845894536da481f6e78a8ab3
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-16 04:01:54 +0600

    Completed_Counter_App

---
+++ Counting App full/index.js
@@ -0,0 +1,22 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl= document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countStr= count + "-"
+
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText+= countStr
+    //for setting the count again from 0
+     countEl.textContent=0
+     count=0
+}
+
+
