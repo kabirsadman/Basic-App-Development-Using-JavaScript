2c292fa935bc289014af4223bdf3fd7aad68e2ea
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-16 01:58:16 +0600

    Simple_Counting_App

---
+++ Counting App/index.js
@@ -0,0 +1,43 @@
+// document.getElementById("count").innerText = 5
+
+// let count = 5
+
+// console.log(count)
+// let myAge=35
+// let dogAgeRatio=7
+// let myDogAge= myAge*dogAgeRatio
+// console.log(myDogAge)
+// let bonusPoints=50
+// console.log(bonusPoints)
+// bonusPoints= bonusPoints + 50
+//  console.log(bonusPoints)
+//  bonusPoints= bonusPoints - 75
+//  console.log(bonusPoints)
+//  bonusPoints= bonusPoints + 45
+//  console.log(bonusPoints)
+//  function increment()
+//  {
+//      console.log("This button is just clicked")
+//  }
+ 
+
+// let lapsCompleted= 0
+// function increment()
+// {
+//     lapsCompleted= lapsCompleted + 1
+//     console.log(lapsCompleted)
+// }
+// increment()
+
+//for counting after every clicj starts from here
+let countEl=document.getElementById("count")
+let count = 0
+function increment()
+{
+    count= count + 1
+    countEl.innerText= count
+    //console.log(count)
+    
+}
+
+
