01af5a9b1bcc56a25327913073b14f75baf7f048
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-17 04:10:28 +0600

    $ git commit -m variables_concatinate_increment_decrement_add_function_remove_function

---
+++ Basics/basic.js
@@ -0,0 +1,48 @@
+////variable practice
+// Create two variables, firstName and lastName
+let firstName= "Sadman"
+let lastName="Sami"
+// Concatenate the two variables into a third variable called fullName
+let fullName= firstName + " "+lastName
+
+// Log fullName to the console
+console.log(fullName)
+
+////Concatenate two string in a function
+let name = "Linda"
+let greeting = "Hi there"
+
+// Create a function that logs out "Hi there, Linda!" when called
+function String()
+{
+    let lol=greeting+","+" "+name+"!"
+    console.log(lol)
+}
+String()
+
+////incrementing and decrementing
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+function add3Points()
+{
+    myPoints+=3
+    
+}
+function remove1Point()
+{
+    myPoints-=1
+}
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+// add/remove points to/from the myPoints variable
+
+
+
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
+
