b125093a707a6356d3e8f48c096b095691ae6fb7
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-29 23:50:21 +0600

    Text_Manipulating_After_Clicking_Button

--- Statement/index.js
+++ Statement/index.js
@@ -1,16 +1,21 @@
-// Check if the person is elegible for a birthday card from the King! (100)
+let firstCard = 20
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+// 1. Create a variable called isAlive and assign it to true
+let isAlive= true
 
-let age = 100
+// 2. Flip its value to false in the appropriate code block 
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("You're out of the game! 😭")
+    isAlive=false
+}
 
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
-if(age<100){
-    console.log("Not elegible")
-}
-else if(age===100){
-    console.log("Here is your birthday card from the king")
-}
-else{
-    console.log("Not elegible,you have already gotten one")
-}
+
+// 3. Log it out to check that you're doing it right
+console.log(isAlive)
