2828a4fd8bb299d3ac9a165f68c8641611fbe34a
kabirsadman <sadmankabirsami9@gmail.com>
2022-04-03 16:29:26 +0600

    For_Adding_Use_push

---
+++ Adding items from array/index.js
@@ -0,0 +1,10 @@
+let cards = [7, 4]
+let messages = ["Hello", "My X keyword is not working propoerly", "I am 23 years old"]
+let newMessage= "I am back!"
+messages.push(newMessage)
+console.log(messages)
+
+
+
+
+
